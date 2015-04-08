f1f48ad3f4a65930fd0896af0928a053e8502839
Anand Patil <abpsap@gmail.com>
2015-04-07 20:54:08 -0400

    Modified for Assignment 1

--- learning_journal/models.py
+++ learning_journal/models.py
@@ -18,10 +18,18 @@ DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
 
 
-class MyModel(Base):
-    __tablename__ = 'models'
-    id = Column(Integer, primary_key=True)
-    name = Column(Text)
-    value = Column(Integer)
+#class MyModel(Base):
+#    __tablename__ = 'models'
+#    id = Column(Integer, primary_key=True)
+#    name = Column(Text)
+#    value = Column(Integer)
 
-Index('my_index', MyModel.name, unique=True, mysql_length=255)
+class Entry(Base):
+    __tablename__ = 'entries'
+    id = Column(Integer, primary_key=True)
+    title = Column(Text, String(255), nullable=False)
+    body = Column(Text, nullable = True)
+    created = Column(DateTime, default=datetime.datetime.utcnow)
+    edited = Column(DateTime, default=datetime.datetime.utcnow)
+
+Index('my_index', Entry.title, unique=True, mysql_length=255)
